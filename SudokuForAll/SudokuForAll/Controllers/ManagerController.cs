a123a1703a994015dfccc09a6648f23de4bfec1c
efrainmejiasc <efrainmejiasc@hotmail.com>
2019-08-26 08:48:56 -0400

    ManagerController

---
+++ SudokuForAll/SudokuForAll/Controllers/ManagerController.cs
@@ -0,0 +1,36 @@
+﻿using SudokuForAll.Engine;
+using SudokuForAll.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SudokuForAll.Controllers
+{
+    public class ManagerController : Controller
+    {
+        private IEngineDb Metodo;
+        private IEngineProyect Funcion;
+        private IEngineNotificacion Notificacion;
+        private readonly SudokuContext Context;
+
+        public ManagerController(IEngineDb _Metodo, IEngineProyect _Funcion, IEngineNotificacion _Notificacion, SudokuContext _Context)
+        {
+            this.Metodo = _Metodo;
+            this.Context = _Context;
+            this.Funcion = _Funcion;
+            this.Notificacion = _Notificacion;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+    }
+}

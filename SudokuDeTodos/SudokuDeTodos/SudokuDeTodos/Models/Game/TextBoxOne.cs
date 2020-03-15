7077220b1795ce83f66847ca46a09da7d2e390ba
efrainmejiasc <efrainmejiasc@hotmail.com>
2020-03-15 12:55:50 -0400

    opdate

---
+++ SudokuDeTodos/SudokuDeTodos/SudokuDeTodos/Models/Game/TextBoxOne.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SudokuDeTodos.Models.Game
+{
+    public class TextBoxOne
+    {
+        //FILA 0
+        public string txt00 { get; set; }   
+        public string txt01 { get; set; }
+        public string txt02 { get; set; } 
+        public string txt03 { get; set; } 
+        public string txt04 { get; set; } 
+        public string txt05 { get; set; } 
+        public string txt06 { get; set; }
+        public string txt07 { get; set; }
+        public string txt08 { get; set; }
+
+        //FILA 1
+        public string txt10 { get; set; }
+        public string txt11 { get; set; }
+        public string txt12 { get; set; }
+        public string txt13 { get; set; }
+        public string txt14 { get; set; }
+        public string txt15 { get; set; }
+        public string txt16 { get; set; }
+        public string txt17 { get; set; }
+        public string txt18 { get; set; }
+
+        //FILA 2
+        public string txt20 { get; set; }
+        public string txt21 { get; set; }
+        public string txt22 { get; set; }
+        public string txt23 { get; set; }
+        public string txt24 { get; set; }
+        public string txt25 { get; set; }
+        public string txt26 { get; set; }
+        public string txt27 { get; set; }
+        public string txt28 { get; set; }
+
+        //FILA 3
+        public string txt30 { get; set; }
+        public string txt31 { get; set; }
+        public string txt32 { get; set; }
+        public string txt33 { get; set; }
+        public string txt34 { get; set; }
+        public string txt35 { get; set; }
+        public string txt36 { get; set; }
+        public string txt37 { get; set; }
+        public string txt38 { get; set; }
+
+        //FILA 4
+        public string txt40 { get; set; }
+        public string txt41 { get; set; }
+        public string txt42 { get; set; }
+        public string txt43 { get; set; }
+        public string txt44 { get; set; }
+        public string txt45 { get; set; }
+        public string txt46 { get; set; }
+        public string txt47 { get; set; }
+        public string txt48 { get; set; }
+
+        //FILA 5
+        public string txt50 { get; set; }
+        public string txt51 { get; set; }
+        public string txt52 { get; set; }
+        public string txt53 { get; set; }
+        public string txt55 { get; set; }
+        public string txt54 { get; set; }
+        public string txt56 { get; set; }
+        public string txt57 { get; set; }
+        public string txt58 { get; set; }
+
+        //FILA 6
+        public string txt60 { get; set; }
+        public string txt61 { get; set; }
+        public string txt62 { get; set; }
+        public string txt63 { get; set; }
+        public string txt66 { get; set; }
+        public string txt64 { get; set; }
+        public string txt65 { get; set; }
+        public string txt67 { get; set; }
+        public string txt68 { get; set; }
+
+        //FILA 7
+        public string txt70 { get; set; }
+        public string txt71 { get; set; }
+        public string txt72 { get; set; }
+        public string txt73 { get; set; }
+        public string txt74 { get; set; }
+        public string txt75 { get; set; }
+        public string txt76 { get; set; }
+        public string txt77 { get; set; }
+        public string txt78 { get; set; }
+
+        //FILA 8
+        public string txt80 { get; set; }
+        public string txt81 { get; set; }
+        public string txt82 { get; set; }
+        public string txt83 { get; set; }
+        public string txt84 { get; set; }
+        public string txt85 { get; set; }
+        public string txt86 { get; set; }
+        public string txt87 { get; set; }
+        public string txt88 { get; set; }
+    }
+}
